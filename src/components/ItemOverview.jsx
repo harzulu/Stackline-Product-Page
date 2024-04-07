82743836084636a692a7c3722d08d1a8e3cba213
harzulu <benrmcfarland@gmail.com>
2024-04-07 11:33:28 -0700

    setup item overview

--- src/components/ItemOverview.jsx
+++ src/components/ItemOverview.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
-const ItemOverview = () => {
+const ItemOverview = ({ data, loading, error }) => {
+  if (loading || !data[0]) return(<div>Loading...</div>)
+
+  if (error) return(<div>Error: {error}</div>)
+
+  const { image, subtitle, tags, title } = data[0]
+  
   return(
-    <div className="item-overview"></div>
+    <div className="item-overview">
+      <img className="product-image" src={image} alt={title} />
+      <p className="title">{title}</p>
+      <p className="subtitle">{subtitle}</p>
+      <div className="tags-container">
+        {tags.map((tag, i) => (
+          <div className="tag" key={i}>
+            <p className="tag=text">{tag}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
-export default ItemOverview
+const mapStateToProps = (state) => ({
+  data: state.data.data,
+  loading: state.data.loading,
+  error: state.data.error
+})
+
+export default connect(mapStateToProps)(ItemOverview)
