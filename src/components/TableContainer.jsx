bca7990c64a52524227a45804ae9fa53814f0b17
harzulu <benrmcfarland@gmail.com>
2024-04-07 11:57:08 -0700

    setup sale table

--- src/components/TableContainer.jsx
+++ src/components/TableContainer.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
-const TableContainer = () => {
+const formatDate = (dateString) => {
+  const [year, month, day] = dateString.split("-")
+  return `${month}-${day}-${year}`
+}
+
+const TableContainer = ({ data, loading, error }) => {
+  if (loading || !data[0]) return(<div>Loading...</div>)
+
+  if (error) return(<div>Error: {error}</div>)
+
+  const sales = data[0].sales
+
   return (
-    <div className="table-container"></div>
+    <div className="table-container">
+      <div className="sales-table-labels">
+        <p>Week Ending</p>
+        <p>Retail Sales</p>
+        <p>Wholesale Sales</p>
+        <p>Units Sold</p>
+        <p>Retailer Margin</p>
+      </div>
+      <div className="sales-rows-container">
+        {sales.map((sale, i) => (
+          <div className="sale-row-container" key={i}>
+            <p>{formatDate(sale.weekEnding)}</p>
+            <p>{sale.retailSales}</p>
+            <p>{sale.wholesaleSales}</p>
+            <p>{sale.unitsSold}</p>
+            <p>{sale.retailerMargin}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
-export default TableContainer
+const mapStateToProps = (state) => ({
+  data: state.data.data,
+  loading: state.data.loading,
+  error: state.data.error
+})
+
+export default connect(mapStateToProps)(TableContainer)
